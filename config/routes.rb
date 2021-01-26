e1cde7541adc10efb760c54df69a8168088d9be6
udberg <johnuduak066@gmail.com>
2021-01-26 11:34:57 +0100

    Add Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+ root 'static_pages#home'
+  get '/help', to:'static_pages#help'
+  get '/about', to:'static_pages#about'
 end
